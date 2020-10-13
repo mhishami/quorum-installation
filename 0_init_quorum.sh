e05f6634d1cef2d4c55feb8029b6ff1f875bc9b9
Hisham Ismail <mhishami@gmail.com>
2020-10-13 23:19:17 +0800

    Fixed bash

--- 0_init_quorum.sh
+++ 0_init_quorum.sh
@@ -1,12 +1,17 @@
-
+#!/bin/sh
+#
 wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
+sudo tar -C /usr/local/lib -xzf go1.15.2.linux-amd64.tar.gz
 
 sudo apt install -y build-essential
 
-export PATH=$PATH:/usr/local/go/bin
+echo > ~/.bash_profile <EOF
+GO_HOME="/usr/local/lib/go"
+QUORUM_HOME="/usr/local/lib/quorum"
+PATH="$QUORUM_HOME/build/bin:$GO_HOME/bin:$PATH"
 
-echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bash_profile
+export PATH GO_HOME QUORUM_HOME
+EOF
 . ~/.bash_profile
 
 git clone https://github.com/ConsenSys/quorum.git
@@ -14,9 +19,7 @@ cd quorum
 make all
 cd ..
 
-sudo mv quorum /usr/local/.
+sudo mv quorum /usr/local/lib/.
 
-echo "QUORUM_HOME=/usr/local/quorum" >> ~/.bash_profile
-echo "export PATH=\$QUORUM_HOME/build/bin:\$PATH" >> ~/.bash_profile
 . ~/.bash_profile
 
