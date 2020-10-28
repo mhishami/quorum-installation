2a544543de73121849f9801750a818ddf3e6e590
Hisham Ismail <mhishami@gmail.com>
2020-10-28 12:55:25 +0800

    Added gitignore

--- 4_init_data.sh
+++ 4_init_data.sh
@@ -10,21 +10,3 @@ for n in `seq 0 ${nodes}`; do
 	cd ${CURR}
 done
 
-exit
-
-cd nodes/node0
-geth --datadir data init genesis.json
-
-cd ../node1
-geth --datadir data init genesis.json
-
-cd ../node2
-geth --datadir data init genesis.json
-
-cd ../node3
-geth --datadir data init genesis.json
-
-cd ../node4
-geth --datadir data init genesis.json
-
-cd ..
